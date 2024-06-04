347a386d2930660f784e82073f87a263816ee822
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2024-06-04 22:30:03 +0200

    Add Bert interface

---
+++ models/bert.py
@@ -0,0 +1,12 @@
+from sklearn.base import BaseEstimator
+
+class BertClassifier(BaseEstimator):
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
