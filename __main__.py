df5b236a0675843ba0ea6ad7b5b469c67abb0939
Valerio Di Pasquale <40913709+daqh@users.noreply.github.com>
2024-06-23 14:56:58 +0200

    Updated models tests

--- __main__.py
+++ __main__.py
@@ -6,45 +6,58 @@ from models.decision_tree import decision_tree
 from models.mnb import mnb
 from models.knn import knn
 from models.svm import svc
-# from models.bert import bert_classifier
+from models.bert import bert_classifier
 from time import time
 
-df = pd.read_csv('data/Phishing_Email.csv')
-df = df.dropna()
+def main():
+    df = pd.read_csv('data/Phishing_Email.csv')
+    df = df.dropna()
 
-X = df['email'].to_numpy()
-y = df['label'].to_numpy()
+    X = df['email'].to_numpy()
+    y = df['label'].to_numpy()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-print('Random Forest')
-random_forest.fit(X_train, y_train)
-y_pred = random_forest.predict(X_test)
-print(classification_report(y_test, y_pred))
+    begin = time()
+    print('Random Forest')
+    random_forest.fit(X_train, y_train)
+    y_pred = random_forest.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    print('Time taken:', time() - begin)
 
-print('Multinomial Naive Bayes')
-mnb.fit(X_train, y_train)
-y_pred = mnb.predict(X_test)
-print(classification_report(y_test, y_pred))
+    begin = time()
+    print('Multinomial Naive Bayes')
+    mnb.fit(X_train, y_train)
+    y_pred = mnb.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    print('Time taken:', time() - begin)
 
-print('K-Nearest Neighbors')
-knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
-print(classification_report(y_test, y_pred))
+    begin = time()
+    print('K-Nearest Neighbors')
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    print('Time taken:', time() - begin)
 
-print('SVC')
-svc.fit(X_train, y_train)
-y_pred = svc.predict(X_test)
-print(classification_report(y_test, y_pred))
+    begin = time()
+    print('SVC')
+    svc.fit(X_train, y_train)
+    y_pred = svc.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    print('Time taken:', time() - begin)
 
-print('Decision Tree')
-decision_tree.fit(X_train, y_train)
-y_pred = decision_tree.predict(X_test)
-print(classification_report(y_test, y_pred))
+    begin = time()
+    print('Decision Tree')
+    decision_tree.fit(X_train, y_train)
+    y_pred = decision_tree.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    print('Time taken:', time() - begin)
 
-# print('Bert')
-# y_pred = bert_classifier.predict(X_test)
-# print(classification_report(y_test, y_pred))
+    begin = time()
+    print('Bert')
+    y_pred = bert_classifier.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    print('Time taken:', time() - begin)
 
-# end = time()
-# print(f'Time: {end - begin}')
+if __name__ == '__main__':
+    main()
